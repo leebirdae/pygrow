ff786af6fda3480909efad4dd82a403e1f1fca54
leebirdae <Watklm4@gmail.com>
2016-02-10 18:24:38 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pygrow',
+      version='0.0',
+      description='Automated growing of plants',
+      url='https://github.com/leebirdae/pygrow',
+      author='leebirdae',
+      author_email='watklm4@gmail.com',
+      packages=['pygrow'],
+      zip_safe=False)
